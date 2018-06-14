0885ad0edd86e9a999b79bc9e083366a53e7072b
Caleb Webber <caleb@MOTHERSHIP>
2018-06-13 20:00:57 -0400

    Initial implementation of LogEntry class.

--- authlog-ip-extract.py
+++ authlog-ip-extract.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
 import os
 import re
-import pandas as pd
-import numpy as np
 import sys
-regex = '^(\D{3}\s+\d{1,2}\s+.{8}).+(Accepted|Failed) password for (\S+) from (\S+)'
 
 if len(sys.argv) <= 1:
     print("USAGE: authlog-ip-extract /path/to/auth.log")
 
-print(sys.argv[1])
+class LogEntry:
+    _regex = "^(\D{3}\s+\d{1,2}\s+.{8}).+(Accepted|Failed) password for (\S+) from (\S+)"
+    def BuildEntry(log_text):
+        temp = LogEntry()
+        re_match = re.match(LogEntry._regex, log_text)
+        #if not re_match:
+        if not re_match:
+            return None
+        temp._log_dt = re_match.group(1) 
+        temp._log_status = re_match.group(2)
+        temp._log_user = re_match.group(3)
+        temp._log_ip = re_match.group(4)
+        return temp
+
 file = open(sys.argv[1], 'r')
-login_df = pd.DataFrame()
 
+log_entries = [] 
 for line in file:
-    lm = re.match(regex, line)
-    if not lm:
-        continue
-    login_df = login_df.append({'Date':lm.group(1), 'Status':lm.group(2), 
-                     'Username':lm.group(3), 'IP Address':lm.group(4)}, ignore_index = True)
+    temp = LogEntry.BuildEntry(line)
+    if temp is not None:
+        log_entries.append(temp)
 
-
-login_df['Date'] = pd.to_datetime(str(pd.Timestamp.today().year) + login_df['Date'],
-                                   format='%Y%b %d %H:%M:%S', errors='ignore')
-
-GB = login_df.groupby(['Username', 'IP Address', 'Status'])
-m = pd.DataFrame(GB.agg({'Date': np.max,
-        'Status': 'count'}))
-
-print(m)
+for entry in log_entries:
+    print(entry)
