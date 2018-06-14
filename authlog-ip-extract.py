902b7edae61f6e5572f121a816d585dc8a929896
Caleb Webber <seeplusplus@users.noreply.github.com>
2018-06-13 22:55:54 -0400

    Merging new local project with github repo

--- authlog-ip-extract.py
+++ authlog-ip-extract.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os
 import re
 import sys
 
@@ -9,12 +8,13 @@ if len(sys.argv) <= 1:
 else:
     log_file = sys.argv[1]
 
+
 class LogEntry:
     _regex = "^(\D{3}\s+\d{1,2}\s+.{8}).+(Accepted|Failed) password for (\S+) from (\S+)"
-    def BuildEntry(log_text):
+
+    def build_entry(log_text):
         temp_entry = LogEntry()
         re_match = re.match(LogEntry._regex, log_text)
-        #if not re_match:
         if not re_match:
             return None
         temp_entry._log_dt = re_match.group(1) 
@@ -22,10 +22,12 @@ class LogEntry:
         temp_entry._log_user = re_match.group(3)
         temp_entry._log_ip = re_match.group(4)
         return temp_entry
+
     def __str__(self):
         return f"{self._log_dt} {self._log_status} {self._log_user} {self._log_ip}"
 
-def BuildLogFromFile(file_name):
+
+def build_log_from_file(file_name):
     file = open(file_name, 'r')
 
     log_entries = [] 
@@ -36,19 +38,22 @@ def BuildLogFromFile(file_name):
             log_entries.append(temp)
     return log_entries
 
-def PrintReport(log_list):
+
+def print_report(log_list):
     report_str = "" 
     for status in sorted({l._log_status for l in log_list}): # for each unique status
         for user in sorted({l._log_user for l in log_list}): # for each unique user
             report_str += f"{status} from {user}\n"
             user_status_ip_sum = {}
             for ip in sorted({l._log_ip for l in log_list}): # for each unique ip
-                user_status_ip_sum[ip] = sum(1 for l in log_list if l._log_status == status and l._log_user == user and l._log_ip == ip)
+                user_status_ip_sum[ip] = sum(1 for l in log_list if l._log_status == status and l._log_user == user and
+                                             l._log_ip == ip)
                 if user_status_ip_sum[ip]:
                     report_str += f"\t{user_status_ip_sum[ip]} from {ip}\n"
             report_str += f"Total {sum(user_status_ip_sum.values())}\n"
     print(report_str)
 
-log_list = BuildLogFromFile(log_file)
-PrintReport(log_list)
+
+log_list = build_log_from_file(log_file)
+print_report(log_list)
 
